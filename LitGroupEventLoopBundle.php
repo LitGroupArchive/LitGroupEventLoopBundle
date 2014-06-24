6bfca0ce94a2241455ccb00fea1f477fcbd8cbbb
Roman Shamritskiy <roman@litgroup.ru>
2014-06-24 18:17:57 +0400

    Initial commit.

---
+++ LitGroupEventLoopBundle.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * This file is part of the "LitGroupEventLoopBundle" package.
+ *
+ * (c) Roman Shamritskiy <roman@litgroup.ru>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace LitGroup\Bundle\EventLoopBundle;
+
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+/**
+ * LitGroupEventLoopBundle
+ *
+ * @author Roman Shamritskiy <roman@litgroup.ru>
+ */
+class LitGroupEventLoopBundle extends Bundle
+{
+    public function build(ContainerBuilder $container)
+    {
+        parent::build($container);
+    }
+
+}
